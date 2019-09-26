23ab22195c877412c07d375cd001ecd1043ef486
NAGARRO\divakarjoshi <divakar.joshi@nagarro.com>
2019-09-26 17:34:32 +0530

    Updated

--- webui/src/test/java/autothon/webui/tests/BaseTest.java
+++ webui/src/test/java/autothon/webui/tests/BaseTest.java
@@ -2,6 +2,7 @@ package autothon.webui.tests;
 
 import java.lang.reflect.Method;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -11,20 +12,27 @@ import autothon.webui.pages.BasePage;
 import autothon.webui.utils.Reporting;
 
 public class BaseTest {
+	final static Logger logger = Logger.getLogger(BaseTest.class);
+	
 	 protected static Reporting oReporting = new Reporting();
 	 protected static BasePage oBasepage = new BasePage();
 	 @BeforeMethod
 	  public void beforeMethod(Method method) {
 		 oReporting.setTestCaseName(method.getName());
+	
+		 
 	  }
 
 	  @AfterMethod
 	  public void afterMethod() {
+		  oBasepage.closeDriver();
 	  }
 
 	  @BeforeSuite
 	  public void beforeSuite() {
 		  oReporting.startReporting();
+		  
+		  
 	  }
 
 	  @AfterSuite
